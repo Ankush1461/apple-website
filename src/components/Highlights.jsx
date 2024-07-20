f82412041dc7171cf3c13a428beecea251a480dd
Ankush Karmakar <karmakarankush1461@gmail.com>
2024-07-20 07:20:46 +0000

    Version 1.0.1 - Minor fixes

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -20,12 +20,12 @@ const Highlights = () => {
           <h1 id="title" className="section-heading">
             Get the highlights.
           </h1>
-          <div className="flex flex-wrap items-end gap-5">
+          <div className="flex flex-wrap items-end md:flex-row flex-col gap-5">
             <p className="link">
               Watch the film <img src={watchImg} alt="watch" className="ml-2" />
             </p>
             <p className="link">
-              Watch the event{" "}
+              Watch the event
               <img src={rightImg} alt="right" className="ml-2" />
             </p>
           </div>
